6094132db33f827c6f1845ff20d4c040110f5cfd
Daniel Newman <dan@cubicapps.com>
2014-07-31 18:30:30 +0100

    Package NGINX and PHP on Heroku and upload to dropbox
    Use `$ heroku run bash --app <app_name>` to create a one-off dyno. Execute the desired package to build a new binary and upload to dropbox e.g., `$ bash support/heroku_package_nginx.sh`.

---
+++ support/heroku_package_nginx.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+
+if [ "$NGINX_VERSION" == "" ]; then
+  echo "must set NGINX_VERSION environment variable, i.e. $ export NGINX_VERSION=1.7.3"
+  exit 1
+fi
+if [ "$PCRE_VERSION" == "" ]; then
+  echo "must set PCRE_VERSION environment variable, i.e. $ export PCRE_VERSION=8.35"
+  exit 1
+fi
+
+# download and extract nginx
+rm -rf /app/vendor/nginx
+curl http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz -o nginx.tgz
+cd /tmp
+tar xzvf nginx.tgz
+
+# download and extract pcre into contrib directory
+cd nginx-$NGINX_VERSION/contrib
+curl ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-$PCRE_VERSION.tar.gz -o pcre.tgz
+tar zvxf pcre.tgz
+
+# build and package nginx for heroku
+cd /tmp/nginx-$NGINX_VERSION
+./configure --prefix=/app/vendor/nginx \
+		   --with-pcre=contrib/pcre-$PCRE_VERSION \
+		   --with-http_ssl_module \
+		   --with-http_gzip_static_module \
+		   --with-http_stub_status_module \
+		   --with-http_realip_module
+make install
+cd /app/vendor/nginx
+tar cvfz /tmp/nginx-$NGINX_VERSION-heroku.tar.gz .
+
+# upload to dropbox using Dropbox-Uploader
+cd /tmp/
+bash /app/support/drop/dropbox_uploader.sh upload nginx-1.7.3-heroku.tar.gz /nginx/nginx-1.7.3-heroku.tar.gz
---
+++ support/heroku_package_php.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+set -e
+
+if [ "$PHP_VERSION" == "" ]; then
+  echo "must set PHP_VERSION environment variable, i.e. $ export PHP_VERSION=5.5.15"
+  exit 1
+fi
+
+# download and extract php
+rm -rf /app/vendor/php
+curl -L http://uk1.php.net/get/php-$PHP_VERSION.tar.bz2/from/this/mirror -o /tmp/php.tar.bz2
+cd /tmp
+tar xvjf php.tar.bz2
+
+# build and package php for heroku
+cd php-$PHP_VERSION
+./configure --prefix=/app/vendor/php \
+			--with-mysql \
+			--with-pdo-mysql \
+			--with-iconv \
+			--with-gd \
+			--with-curl=/usr/lib \
+			--with-config-file-path=/app/vendor/php \
+			--with-openssl \
+			--enable-fpm \
+			--with-zlib \
+			--enable-mbstring \
+			--disable-debug \
+			--disable-rpath \
+			--enable-gd-native-ttf \
+			--enable-inline-optimization \
+			--with-bz2 \
+			--enable-pcntl \
+			--enable-mbregex \
+			--with-mhash \
+			--enable-zip \
+			--with-pcre-regex \
+			--enable-libxml \
+			--with-gettext \
+			--with-jpeg-dir \
+			--with-mysqli \
+			--with-pcre-regex \
+			--with-png-dir \
+			--without-pdo-sqlite \
+			--without-sqlite3
+make install
+/app/vendor/php/bin/pear config-set php_dir /app/vendor/php
+yes '' | /app/vendor/php/bin/pecl install memcache
+cd /app/vendor/php
+tar cvfz /tmp/php-$PHP_VERSION-with-fpm-heroku.tar.gz .
+
+# upload to dropbox using Dropbox-Uploader
+cd /tmp/
+bash /app/support/drop/dropbox_uploader.sh upload php-5.5.15-with-fpm-heroku.tar.gz /php/php-5.5.15-with-fpm-heroku.tar.gz
